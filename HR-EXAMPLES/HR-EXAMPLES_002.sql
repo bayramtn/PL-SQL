8aaf14ed2fb52c688ca3c5e0898758302383ad4a
bayramtn <77554408+bayramtn@users.noreply.github.com>
2021-01-29 14:05:04 +0300

    ADDED EXAMPLES-3

--- HR-EXAMPLES/HR-EXAMPLES_002.sql
+++ HR-EXAMPLES/HR-EXAMPLES_002.sql
@@ -1,29 +1,29 @@
 SELECT 
-employee_ýd,fýrst_name,last_name,job_ýd,manager_ýd,department_ýd,LEVEL 
+employee_id,first_name,last_name,job_id,manager_id,department_id,LEVEL 
 FROM employees
-CONNECT BY PRIOR employee_id = manager_id
-START WITH manager_ýd IS NULL ORDER BY LEVEL;
+CONNECT BY PRiOR employee_id = manager_id
+START WiTH manager_id iS NULL ORDER BY LEVEL;
 
 SELECT 
-employee_ýd,fýrst_name,last_name,job_ýd,manager_ýd,department_ýd
-,(select fýrst_name from employees e where e.employee_id = emp.manager_id)as boss
+employee_id,first_name,last_name,job_id,manager_id,department_id
+,(select first_name from employees e where e.employee_id = emp.manager_id)as boss
 FROM 
 employees emp  ORDER BY 1;
 
 SELECT 
-employee_ýd,fýrst_name,last_name,job_ýd,manager_ýd,department_ýd,LEVEL ,
-(select fýrst_name from employees e where e.employee_id = emp.manager_id)as boss
+employee_id,first_name,last_name,job_id,manager_id,department_id,LEVEL ,
+(select first_name from employees e where e.employee_id = emp.manager_id)as boss
 FROM employees emp
-CONNECT BY PRIOR employee_id = manager_id
-START WITH manager_ýd IS NULL ORDER BY LEVEL;
+CONNECT BY PRiOR employee_id = manager_id
+START WiTH manager_id iS NULL ORDER BY LEVEL;
 
 
 
 SELECT 
-employee_ýd,fýrst_name,last_name,job_ýd,manager_ýd,department_ýd,LEVEL ,
-sys_connect_by_path(fýrst_name,'/') as HIER
+employee_id,first_name,last_name,job_id,manager_id,department_id,LEVEL ,
+sys_connect_by_path(first_name,'/') as HiER
 FROM employees
-CONNECT BY PRIOR employee_id = manager_id
-START WITH manager_ýd IS null ORDER BY LEVEL;
+CONNECT BY PRiOR employee_id = manager_id
+START WiTH manager_id iS null ORDER BY LEVEL;
 
 
